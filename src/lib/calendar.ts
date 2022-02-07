b4efe6b0ef4846ee4c504ac416f005b534780104
Tyler Savery <tyler@theyoungastronauts.com>
2022-02-07 17:08:41 -0500

    ical update'

--- src/lib/calendar.ts
+++ src/lib/calendar.ts
@@ -11,26 +11,40 @@ const _addToGoogle = (name: string, date: string, description: string) => {
 };
 
 const _addToICal = (name: string, date: string, description: string) => {
-  const d = date.split("/");
+  // window.open(require("../misc/rbx.ics"));
+  const link = document.createElement("a");
+  link.href = require("../misc/rbx.ics");
+  link.click();
+  return;
 
-  const url = `data:text/calendar;charset=utf8,BEGIN:VCALENDAR
-VERSION:2.0
-BEGIN:VEVENT
-URL:https://reserveblock.io
-DTSTART:${d[0]}
-DTEND:${d[1]}
-SUMMARY:${name}
-DESCRIPTION:${description}
-LOCATION:
-END:VEVENT
-END:VCALENDAR`;
+  //   const d = date.split("/");
 
-  window.open(url);
+  //   const url = `data:text/calendar;charset=utf8,BEGIN:VCALENDAR
+  // VERSION:2.0
+  // BEGIN:VEVENT
+  // URL:https://reserveblock.io
+  // DTSTART:${d[0]}
+  // DTEND:${d[1]}
+  // SUMMARY:${name}
+  // DESCRIPTION:${description}
+  // LOCATION:
+  // END:VEVENT
+  // END:VCALENDAR`;
+
+  // console.log(url);
+
+  // window.location.href = url;
+
+  // const blob = new Blob([url], { type: "text/calendar" });
+  // const link = document.createElement("a");
+  // link.href = window.URL.createObjectURL(blob);
+  // link.download = "rbx.ics";
+  // link.click();
 };
 
 export const addToCalendar = (type: CalendarType) => {
   const name = "RBX Node Presale";
-  const date = "2022-02-22-T23-00-00Z/2022-02-22-T24-00-00Z";
+  const date = "2022-02-22-T23-00-00Z/2022-02-23-T00-00-00Z";
   const description = "DEMOCRATIZING NFTs FOR EVERYONE";
 
   const _name = encodeURIComponent(name);

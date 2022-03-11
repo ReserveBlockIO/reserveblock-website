637999e6e7cdfcafa19e27aea1910f1136ed37e4
Mathis <20599614+mathis1337@users.noreply.github.com>
2022-03-11 17:40:53 -0600

    Update github-files.ts

--- src/github-files.ts
+++ src/github-files.ts
@@ -1,13 +1,13 @@
 // CONFIG STARTS
 
 const _cli = {
-  tag: "pre7",
-  date: "March 7, 2022",
+  tag: "pre8",
+  date: "March 11, 2022",
 };
 
 const _gui = {
-  tag: "pre7",
-  date: "March 7, 2022",
+  tag: "pre8",
+  date: "March 11, 2022",
 };
 
 // CONFIG ENDS

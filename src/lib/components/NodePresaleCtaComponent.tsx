615c6268663faf068486725da47bdf0b736c6cdc
Tyler Savery <tyler@theyoungastronauts.com>
2022-07-18 14:20:42 -0400

    update

--- src/lib/components/NodePresaleCtaComponent.tsx
+++ src/lib/components/NodePresaleCtaComponent.tsx
@@ -31,7 +31,7 @@ export const NodePresaleCtaComponent = () => {
           <div className="">
             <SectionHeading4>
               <strong style={{ fontSize: 38, letterSpacing: 3 }}>
-                MAINET BETA LIVE
+                MAINNET BETA LIVE
               </strong>
             </SectionHeading4>
             {/* <SectionHeading4>
